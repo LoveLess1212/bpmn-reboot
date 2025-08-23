05f381be856675e5e39111d490c674e20a7bfcdc
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2025-08-23 23:05:36 +0700

    fix: correct Blockfrost provider configuration

--- src/common/Provider.ts
+++ src/common/Provider.ts
@@ -1,10 +1,11 @@
-import {KoiosProvider} from '@meshsdk/core';
-import {BlockfrostProvider} from "@meshsdk/core";
+import { KoiosProvider } from '@meshsdk/core';
+import { BlockfrostProvider } from '@meshsdk/core';
 
 export const BlockchainProvider = new KoiosProvider(
     'preprod',
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdTl3ejdnanpmcXZ2OG04dHhxaHh0ZTZlOWo1d3Fnc2RrZjByN3JhNDZsZW13eGNkM2o4NnAiLCJleHAiOjE3NTk2NjA4NzgsInRpZXIiOjEsInByb2pJRCI6IlRoZXNpcyJ9.jV_JC7DOPjOQHv-s4DDoZJYCvv6QATdVljA46zmu_U8'
 );
 export const BlockFrostProd = new BlockfrostProvider(
-    'preprodbhlMliCvWTsl9s3dB9YFsiVPmrfaXUak'
+    'preprod',
+    'bhlMliCvWTsl9s3dB9YFsiVPmrfaXUak'
 );
